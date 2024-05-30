967be0bdffee73e9f8e00c1d93d0fe3f59e39fc5
Your Name <you@example.com>
2024-05-29 20:17:45 -0700

    my first commit

--- src/main/java/pet/store/controller/PetStoreController.java
+++ src/main/java/pet/store/controller/PetStoreController.java
@@ -1,7 +1,12 @@
 package pet.store.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,7 +16,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import pet.store.controller.model.PetStoreCustomer;
 import pet.store.controller.model.PetStoreData;
+import pet.store.controller.model.PetStoreEmployee;
 import pet.store.service.PetStoreService;
 
 @RestController
@@ -37,5 +44,59 @@ public class PetStoreController {
 		log.info("Updating pet store {}", petStoreData);
 
 		return petStoreService.savePetStore(petStoreData);
+	}
+
+	@PostMapping("/{petStoreId}/employee")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public PetStoreEmployee insertPetStoreEmployee(@PathVariable Long petStoreId,
+			@RequestBody PetStoreEmployee petStoreEmployee) {
+		log.info("Creating employee {} for petstore with ID=()", petStoreEmployee.getEmployeeId(), petStoreId);
+
+		return petStoreService.saveEmployee(petStoreId, petStoreEmployee);
+	}
+
+	@PutMapping("/{petStoreId}/employee")
+	public PetStoreEmployee updatePetStoreEmployee(@PathVariable Long petStoreId,
+			@RequestBody PetStoreEmployee petStoreEmployee) {
+		log.info("Updating employee {} for petstore with ID=()", petStoreEmployee.getEmployeeId(), petStoreId);
+
+		return petStoreService.saveEmployee(petStoreId, petStoreEmployee);
+	}
+
+	@PostMapping("/{petStoreId}/customer")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public PetStoreCustomer insertPetStoreCustomer(@PathVariable Long petStoreId,
+			@RequestBody PetStoreCustomer petStoreCustomer) {
+		log.info("Creating customer {} for pet store with ID=()", petStoreCustomer.getCustomerId(), petStoreId);
+		return petStoreService.saveCustomer(petStoreId, petStoreCustomer);
+	}
+
+	@PutMapping("/{petStoreId}/customer")
+	public PetStoreCustomer updatePetStoreCustomer(@PathVariable Long petStoreId,
+			@RequestBody PetStoreCustomer petStoreCustomer) {
+		log.info("Updating customer {} for pet store with ID=()", petStoreCustomer.getCustomerId(), petStoreId);
+		return petStoreService.saveCustomer(petStoreId, petStoreCustomer);
+	}
+
+	@GetMapping("/pet_store")
+	public List<PetStoreData> listAllPetStores() {
+		log.info("Listing all pet stores");
+		return petStoreService.retrieveAllPetStores();
+	}
+
+	@GetMapping("/{petStoreId}")
+	public PetStoreData getPetstoreById(@PathVariable Long petStoreId) {
+		log.info("Retrieving pet store with ID=()", petStoreId);
+		return petStoreService.retrievePetStoreById(petStoreId);
+	}
+
+	@DeleteMapping("/{petStoreId}")
+	public Map<String, String> deletePetStoreById(@PathVariable Long petStoreId) {
+		log.info("Received request to delete pet store with ID: " + petStoreId + ".");
+
+		petStoreService.deletePetStoreById(petStoreId);
+
+		return Map.of("Message", "Pet Store with ID=" + petStoreId + "was deleted suscessfully.");
+
 	}
 }
--- src/main/java/pet/store/controller/model/PetStoreData.java
+++ src/main/java/pet/store/controller/model/PetStoreData.java
@@ -41,41 +41,4 @@ public class PetStoreData {
 			employees.add(petStoreEmployee);
 		}
 	}
-
-	@Data
-	@NoArgsConstructor
-	static class PetStoreEmployee {
-
-		private Long employeeId;
-		private String employeeFirstName;
-		private String employeeLastName;
-		private String employeePhone;
-		private String employeeJobTitle;
-
-		public PetStoreEmployee(Employee employee) {
-			employeeId = employee.getEmployeeId();
-			employeeFirstName = employee.getEmployeeFirstName();
-			employeeLastName = employee.getEmployeeLastName();
-			employeePhone = employee.getEmployeePhone();
-			employeeJobTitle = employee.getEmployeeJobTitle();
-		}
-	}
-
-	@Data
-	@NoArgsConstructor
-	static class PetStoreCustomer {
-
-		private Long customerId;
-		private String customerFirstName;
-		private String customerLastName;
-		private String customerEmail;
-
-		public PetStoreCustomer(Customer customer) {
-			customerId = customer.getCustomerId();
-			customerFirstName = customer.getCustomerFirstName();
-			customerLastName = customer.getCustomerLastName();
-			customerEmail = customer.getCustomerEmail();
-
-		}
-	}
 }
